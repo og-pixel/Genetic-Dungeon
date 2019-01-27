585ec2cef96f64e893da339338c4e8caafd3c886
Milosz Jakubanis <milosz.jakubanis@protonmail.com>
2019-01-27 16:15:47 +0000

    Moving files, need to refractor now

--- src/Menu/Main.java
+++ src/Menu/Main.java
@@ -4,8 +4,6 @@ import javafx.application.Application;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Little test to confirm git working");
-        System.out.println("Little test to confirm git working");
         Application.launch(Menu.class, args);
 //        Application.launch(HeroSelect.class, args);
     }
