72f47796f9984bb82b200e1d7e1c2ff0f9233158
Milosz Jakubanis <milosz.jakubanis@protonmail.com>
2019-02-17 18:22:10 +0000

    Refractored and repaired Matrix data structure

--- src/Algorithms/FindAllRooms.java
+++ src/Algorithms/FindAllRooms.java
@@ -2,11 +2,8 @@ package Algorithms;
 
 import Dungeon.Dungeon;
 import Dungeon.Tile.Corridor;
-import com.sun.javafx.scene.traversal.Algorithm;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 
 //todo this class name should not be a verb but I feel like its better than "RoomSize"
 public class FindAllRooms implements Fitness{
@@ -34,33 +31,11 @@ public class FindAllRooms implements Fitness{
 
         for(int y = 0; y < dungeonHeight; y++){
             for(int x = 0; x < dungeonWidth; x++) {
-//                if (dungeon.getDungeonMatrix().getElement(x, y) instanceof Corridor) {
-//                    for(int p = 0; p < listOfRooms.size(); p++){
-//                        if(listOfRooms.get(p).getElement(x, y) && visitMap.getElement(x, y)){
-//                            break;
-//                        }
-//                    }
-//
-//                    try {
-//                        if (!visitMap.getElement(x,y)) {
-//                            visitMap = Algorithms.floodFill(dungeon, x, y);
-//                            if (visitMap != null) { //todo I think its always not null now so i can remove it
-//                                numberOfRooms++;
-//                                printMap(visitMap);
-//                                roomSize = roomSize + countVisited(visitMap);
-//                                listOfRooms.add(visitMap);//todo here
-//                            }
-//                        }
-//                    } catch (Exception e) {
-//                        System.err.println("erros"); //todo i might not need that too
-//                        visitMap = Algorithms.floodFill(dungeon, x, y);
-//                    }
-//                }
 
                boolean alreadyVisited = false;
                if(!listOfRooms.isEmpty()) {
-                   for(int p = 0; p < listOfRooms.size(); p++) {
-                       if (listOfRooms.get(p).getElement(x, y)) {
+                   for (Matrix<Boolean> listOfRoom : listOfRooms) {
+                       if (listOfRoom.getElement(x, y)) {
                            alreadyVisited = true;
                            break;
                        }
@@ -77,12 +52,10 @@ public class FindAllRooms implements Fitness{
                    listOfRooms.add(visitMap);
 
                    numberOfRooms++;
-//                   printMap(visitMap);
                    roomSize = roomSize + countVisited(visitMap);
                }
             }
         }
-
 
         wallCount = (dungeonWidth * dungeonHeight) - roomSize;
         //Currently score is given by calculating ratio of wall to corridor (so 50
