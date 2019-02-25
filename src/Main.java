c2a4fb17e29bcc528c29010e12466a9b31e0956b
Milosz Jakubanis <milosz.jakubanis@protonmail.com>
2019-02-25 19:46:28 +0000

    Updating to match local PC

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 import Algorithms.Algorithms;
 import Algorithms.Matrix;
+import Dungeon.*;
 
 public class Main {
     public static void main(String[] args){
-        new Interpreter(args);
+//        new Interpreter(args);
+        Matrix<Integer> integerMatrix = new Matrix<Integer>(50,50);
+        integerMatrix.fillMatrix(1);
+        System.out.println(integerMatrix);
     }
 }
